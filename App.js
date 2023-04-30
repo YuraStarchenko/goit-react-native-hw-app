9370ab9feee6334ff39b16a6f0abc23bf6e555c4
YuraStarchenko <100568416+YuraStarchenko@users.noreply.github.com>
2023-04-30 16:49:11 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, TextInput, View, ImageBackground } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  ImageBackground,
+  Button,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
   return (
@@ -8,21 +16,31 @@ export default function App() {
         source={require("./assets/PhotoBG.png")}
         style={styles.image}
       >
-        <TextInput
-          style={styles.login}
-          textAlign={"left"}
-          placeholder="Логин"
-        ></TextInput>
-        <TextInput
-          style={styles.mail}
-          textAlign={"left"}
-          placeholder="Адрес электронной почты"
-        ></TextInput>
-        <TextInput
-          style={styles.password}
-          textAlign={"left"}
-          placeholder="Пароль"
-        ></TextInput>
+        <View style={styles.form}>
+          <View>
+            <Text style={styles.inputTitle}>Регистрация</Text>
+            <TextInput
+              style={styles.login}
+              textAlign={"center"}
+              placeholder="Логин"
+            ></TextInput>
+            <TextInput
+              style={styles.mail}
+              textAlign={"center"}
+              placeholder="Адрес электронной почты"
+            ></TextInput>
+            <TextInput
+              style={styles.password}
+              textAlign={"center"}
+              placeholder="Пароль"
+              secureTextEntry={true}
+            ></TextInput>
+          </View>
+
+          <TouchableOpacity activeOpacity={0.8} style={styles.btn}>
+            <Text style={styles.btnTitle}>Зарегистрироваться</Text>
+          </TouchableOpacity>
+        </View>
       </ImageBackground>
       <StatusBar style="auto" />
     </View>
@@ -34,20 +52,25 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
   },
-
   image: {
     flex: 1,
     resizeMode: "cover",
     justifyContent: "center",
-    alignItems: "center",
+    // alignItems: "center",
+  },
+
+  form: {
+    marginHorizontal: 16,
+  },
+
+  inputTitle: {
+    fontSize: 30,
+    fontWeight: 500,
   },
 
   login: {
     borderWidth: 1,
     height: 50,
-		width: 343,
-		marginLeft: 20,
-    marginHorizontal: 20,
     borderRadius: 8,
     borderColor: "#F6F6F6",
 
@@ -58,10 +81,8 @@ const styles = StyleSheet.create({
   mail: {
     borderWidth: 1,
     height: 50,
-    width: 343,
     marginBottom: 16,
     marginTop: 16,
-    marginHorizontal: 20,
     borderRadius: 8,
     borderColor: "#F6F6F6",
 
@@ -72,12 +93,24 @@ const styles = StyleSheet.create({
   password: {
     borderWidth: 1,
     height: 50,
-    width: 343,
-    marginHorizontal: 20,
     borderRadius: 8,
     borderColor: "#F6F6F6",
 
     fontSize: 20,
     color: "#F6F6F6",
+  },
+
+  btn: {
+    backgroundColor: "#FF6C00",
+    height: 51,
+    borderRadius: 100,
+    marginTop: 43,
+    justifyContent: "center",
+		alignItems: "center",
+	},
+	
+  btnTitle: {
+    color: "#ffffff",
+    fontSize: 16,
   },
 });
